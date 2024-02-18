d03f953cbe9fdb76cce1d44a3474a6a79505cf08
Ayush Porwal <87272409+porwal-ayush@users.noreply.github.com>
2024-02-18 14:58:04 +0530

    updating the db to add new table

--- src/index.js
+++ src/index.js
@@ -1,137 +1,36 @@
 import fs from "fs";
 import mysql from "mysql2";
-import { stringify } from "csv-stringify/sync";
-import { S3Client, HeadObjectCommand } from "@aws-sdk/client-s3";
+import csv from "csv-parser";
 
-const bucketName = "wrparchive";
-const accessKeyId = "52d2a666e993b08710c56164c930032d";
-const secretAccessKey =
-  "4a8300b620494f1b3a506c61f70fd43a4c5d6fe88e6105db373f383f0965543d";
-const s3Endpoint =
-  "https://34f62f615789b088af9130d959f70465.r2.cloudflarestorage.com";
+// SQL to create table, manually done in db
+// CREATE TABLE `updatedpictures` (
+//   `pictureID` int NOT NULL,
+//   `type` varchar(20) NOT NULL,
+//   `currentfolder` int NOT NULL
+// ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Cengage$29",
+  password: "password",
   database: "wrptest_2",
 });
 
-const S3 = new S3Client({
-  region: "auto",
-  endpoint: s3Endpoint,
-  credentials: {
-    accessKeyId: accessKeyId,
-    secretAccessKey: secretAccessKey,
-  },
-});
+connection.connect();
 
-function getPictureDataAndExtension() {
-  return new Promise((resolve, reject) => {
-    connection.connect();
-
-    const query = `
-          SELECT pictureID, currentfolder, extension 
-          FROM picture
-      `;
-
-    connection.query(query, (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        const extensionObject = {};
-        const pictureObject = {};
-        results.forEach((row) => {
-          extensionObject[row.pictureID] = row.extension;
-          pictureObject[row.pictureID] = row.currentfolder;
-        });
-        resolve([pictureObject, extensionObject]);
+fs.createReadStream("new_corrected_path.csv")
+  .pipe(csv())
+  .on("data", (row) => {
+    const query = `INSERT INTO updatedpictures (pictureID, type, currentfolder) VALUES (${
+      row.pictureID
+    }, '${row.newPath.split("/")[0]}', '${row.newPath.split("/")[1]}');`;
+    connection.query(query, (err, result) => {
+      if (err) {
+        console.error("Error during insertion:", err);
       }
     });
-    connection.end();
-  });
-}
-
-async function checkPictureExistence(pictureSubfolderMap, extensionObjectMap) {
-  const pictureNotFound = [];
-  const failedPictureIds = new Set();
-  const BATCH_SIZE = 50;
-  let filesProcessed = 0;
-
-  const pathChecks = [];
-
-  for (const pictureID in pictureSubfolderMap) {
-    const subFolder = pictureSubfolderMap[pictureID];
-    const pathsToCheck = [
-      `images/${subFolder}/${pictureID}.${extensionObjectMap[pictureID]}`,
-      `previews/${subFolder}/${pictureID}_s.jpeg`,
-      `thumbnails/${subFolder}/${pictureID}_t.jpeg`,
-    ];
-
-    pathsToCheck.forEach((path) => {
-      pathChecks.push({ pictureID, path });
-    });
-  }
-
-  for (let i = 0; i < pathChecks.length; i += BATCH_SIZE) {
-    const batch = pathChecks.slice(i, i + BATCH_SIZE);
-    const promises = batch.map(({ pictureID, path }) => {
-      const params = { Bucket: bucketName, Key: path };
-      const command = new HeadObjectCommand(params);
-
-      filesProcessed++;
-
-      return S3.send(command)
-        .then(() => {})
-        .catch((err) => {
-          if (err.$metadata && err.$metadata.httpStatusCode === 404) {
-            pictureNotFound.push({ pictureID, pathWhereItsNotFound: path });
-          } else {
-            // network error or cloudflare errors, cant do anything about these except run the job again for these ids
-            console.error(`Error checking ${path}:`, JSON.stringify(err));
-            failedPictureIds.add(pictureID);
-          }
-        });
-    });
-
-    await Promise.all(promises);
-
-    console.log(`Processed ${filesProcessed} files so far...`);
-  }
-  return [pictureNotFound, failedPictureIds];
-}
-
-console.log("Fetching data from db");
-const [pictureSubfolderMap, extensionObjectMap] =
-  await getPictureDataAndExtension();
-
-console.log("Looking for pictures path in r2 bucket");
-checkPictureExistence(pictureSubfolderMap, extensionObjectMap)
-  .then(([missingPictures, failedPictureIds]) => {
-    if (missingPictures.length > 0 || failedPictureIds.size > 0) {
-      if (missingPictures.length > 0) {
-        const missingPicturesCsvData = stringify(missingPictures, {
-          header: true,
-          columns: ["pictureID", "pathWhereItsNotFound"],
-        });
-        fs.writeFileSync("missing_pictures.csv", missingPicturesCsvData);
-      } else {
-        console.log("No pictureIDs are at incorrect path");
-      }
-
-      if (failedPictureIds.size > 0) {
-        const failedPictureIdsCsvData = stringify(
-          Array.from(failedPictureIds).map((id) => ({ pictureID: id })),
-          { header: true }
-        );
-        fs.writeFileSync("failed_picture_ids.csv", failedPictureIdsCsvData);
-      } else {
-        console.log("No pictureIDs failed");
-      }
-    } else {
-      console.log("All pictures found.");
-    }
   })
-  .catch((err) => {
-    console.error("Error running the function:", err);
+  .on("end", () => {
+    console.log("CSV file processed");
+    connection.end();
   });
